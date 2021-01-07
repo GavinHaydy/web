3bacfa25bf4b8d8a9664979f39a150e0eeee46ed
bugpz <13558622779@163.com>
2021-01-07 15:21:01 +0800

    增加调用接口代码及axios

--- src/components/Urlerror.vue
+++ src/components/Urlerror.vue
@@ -1,10 +1,29 @@
 <template>
-  <h1>404 页面正在加急制作</h1>
+<!--  <h1>404 页面正在加急制作</h1>-->
+  <div id="Urlerror">
+    <div v-for="site in info" :key="site">
+      {{site.name}}{{site.info}}
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-  name: 'Urlerror'
+  name: 'Urlerror',
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+      .get('../static/json_demo.json')
+      .then(response => (this.info = response.data.sites))
+      .catch(function (error) {
+        console.log(error)
+      })
+  }
 }
 </script>
 
