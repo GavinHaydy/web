0a37106ddca964d5f45916261a5cc927a9462edd
bugpz <13558622779@163.com>
2021-03-10 23:55:19 +0800

    add：增加register.vue

---
+++ src/pages/register.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Description:register.vue
+ * @CreatedBy:WebStorm
+ * @Author: BugP
+ * @Date: 2021-03-10 23:52
+ * @LastEditTime: 2021-03-10 23:52
+ * @LastEditors: BugP
+-->
+<template>
+  <div>注册页</div>
+</template>
+
+<script>
+export default {
+  name: 'register'
+}
+</script>
+
+<style scoped>
+
+</style>
