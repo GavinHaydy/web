bc0fc1a41a5e22258a8e33957e8a83c431289569
bugpz <13558622779@163.com>
2021-03-11 16:54:05 +0800

    fix：封装请求，有问题

--- src/utils/request.js
+++ src/utils/request.js
@@ -6,3 +6,16 @@
  * @LastEditTime: 2021-03-10 23:51
  * @LastEditors: BugP
 */
+import axios from 'axios'
+
+const service = axios.create({
+  baseURL: '106.13.171.218:82',
+  timeout: 1000
+})
+service.interceptors.response.use(function (response) {
+  if (response.data.code === 200) {
+    return response
+  } else {
+    return response.data.message
+  }
+})
