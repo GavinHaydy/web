c5f8e1e54d1c5ce6cd60942ee59c7c1848f1c46a
bugpz <13558622779@163.com>
2021-03-12 12:55:07 +0800

    add：封装用户注册方法

---
+++ src/api/register.js
@@ -0,0 +1,20 @@
+/*
+ * @Description:register.js
+ * @CreatedBy:WebStorm
+ * @Author: BugP
+ * @Date: 2021-03-12 11:46
+ * @LastEditTime: 2021-03-12 11:46
+ * @LastEditors: BugP
+*/
+import request, {Method} from '../utils/request'
+
+export const userRegister = (data = {}) => {
+  return request({
+    header: {
+      'Content-Type': 'application/json'
+    },
+    url: '/api/register',
+    method: Method.POST,
+    data
+  })
+}
