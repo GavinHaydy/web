9f033e64190662f1711348b06b6c172d165e6ea2
bugpz <13558622779@163.com>
2021-03-12 12:53:55 +0800

    fix：修改超时时间及注释baseURL

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,14 +3,14 @@
  * @CreatedBy:WebStorm
  * @Author: BugP
  * @Date: 2021-03-10 23:51
- * @LastEditTime: 2021-03-10 23:51
+ * @LastEditTime: 2021-3-12 12:52:48
  * @LastEditors: BugP
 */
 import axios from 'axios'
 
 const service = axios.create({
-  baseURL: '106.13.171.218:82',
-  timeout: 1000
+  // baseURL: '106.13.171.218:81',
+  timeout: 5000
 })
 service.interceptors.response.use(function (response) {
   if (response.data.code === 200) {
@@ -19,3 +19,10 @@ service.interceptors.response.use(function (response) {
     return response.data.message
   }
 })
+export const Method = {
+  GET: 'get',
+  POST: 'post',
+  PUT: 'put',
+  DELETE: 'delete'
+}
+export default service
