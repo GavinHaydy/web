304ed744d19c8f8921dfae70dc791fdca826ddce
bugpz <13558622779@163.com>
2021-03-13 13:10:22 +0800

    fix：重置样式

--- src/components/Base.vue
+++ src/components/Base.vue
@@ -1,62 +1,14 @@
+<!--
+ * @Description:Base.vue
+ * @CreatedBy:WebStorm
+ * @Author: BugP
+ * @Date: 2021-01-10 23:52
+ * @LastEditTime: 2021-03-10 23:52
+ * @LastEditors: BugP
+-->
 <template>
-  <div
-    id="Base"
-    :style="{
-      background: type === 1 ? '#f4f4f5' : '#333',
-      color: type === 1 ? '#000' : '#e6a23c'
-    }"
-  >
-    <el-container>
-      <el-header>
-        <div>
-          <span class="left">
-            BUGPZ
-          </span>
-          <div>
-            <a class="a left m_l" href="/">
-              主页
-            </a>
-            <a class="a left m_l" href="/nav">
-              站外导航
-            </a>
-          </div>
-          <div>
-            <el-button
-              :icon="type === 1 ? 'el-icon-turn-off' : 'el-icon-open'"
-              id="bg_cl"
-              class="right col_btn"
-              title="0"
-              @click="handleChange(type)"
-              circle
-            >
-            </el-button>
-            <span class="right">{{ type === 1 ? '浅色' : '深色' }}模式</span>
-            <a href="/register/" class="right a login">
-              注册
-            </a>
-            <a href="/login/" class="right a login">
-              登录
-            </a>
-          </div>
-        </div>
-      </el-header>
-      <el-container>
-        <el-aside>
-          <a
-            href="https://www.aliyun.com/minisite/goods?userCode=qet4zxjy"
-            style="color: crimson"
-            target="_blank"
-          >
-            阿里云产品长期活动
-          </a>
-        </el-aside>
-        <el-main
-          >main
-           <router-view v-if="$route.meta.home"></router-view>
-        </el-main>
-      </el-container>
-    </el-container>
-    <el-backtop :bottom="60"></el-backtop>
+  <div>
+    <div>从头开始</div>
   </div>
 </template>
 
@@ -81,91 +33,5 @@ export default {
 </script>
 
 <style scoped>
-#Base {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  /*height: 100vh;*/
-}
-.col_btn{
-  margin-top: 10px;
-  height: 40px;
-}
 
-.m_l{
-  margin-left: 10px;
-}
-
-.a {
-  color: #42b983;
-}
-.login {
-  margin-right: 10px;
-}
-
-.container {
-  padding-right: 15px;
-  padding-left: 15px;
-  margin-right: auto;
-  margin-left: auto;
-}
-
-.el-header {
-  position: fixed;
-  top: 0;
-  width: 100%;
-  background-color: #b3c0d1;
-  color: #333;
-  text-align: center;
-  line-height: 60px;
-}
-
-.el-footer {
-  position: fixed;
-  bottom: 0;
-  width: 100%;
-  background-color: #b3c0d1;
-  color: #333;
-  text-align: center;
-  height: 6vh;
-  /*line-height: 60px;*/
-}
-
-.el-aside {
-  /*background-color: #d3dce6;*/
-  /*color: #333;*/
-  text-align: center;
-  line-height: 200px;
-  height: 100%;
-}
-
-.el-main {
-  /*background-color: #e9eef3;*/
-  /*color: #333;*/
-  text-align: center;
-  line-height: 160px;
-}
-
-body > .el-container {
-  margin-bottom: 40px;
-}
-
-.el-container:nth-child(5) .el-aside,
-.el-container:nth-child(6) .el-aside {
-  line-height: 260px;
-}
-
-.el-container:nth-child(7) .el-aside {
-  line-height: 320px;
-}
-
-.left {
-  float: left;
-}
-
-.right {
-  float: right;
-}
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,35 +7,18 @@
 -->
 <template>
   <div id="HelloWorld">
-    <h1 v-for="i in 100" :key="i">{{ msg }}</h1>
+    <h1>hh</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+  name: 'HelloWorld'
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1,
-h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
 a {
   color: #42b983;
 }
