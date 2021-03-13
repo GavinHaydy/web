a426d3a534f284f28b74d1fc17b0224de57af240
bugpz <13558622779@163.com>
2021-03-13 10:24:13 +0800

    fix：删除无用路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,13 @@
  * @Description: 路由
  * @Author: zhu
  * @Date: 2020-12-31 15:29:35
- * @LastEditTime: 2021-01-04 11:35:59
+ * @LastEditTime: 2021-03-13 10:23:51
  * @LastEditors: zhu
  */
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '../components/HelloWorld'
 import {Layout} from '../layouts'
-import Navigation from '../components/Navigation'
 import register from '../pages/login/register'
 Vue.use(Router)
 export default new Router({
@@ -32,11 +31,6 @@ export default new Router({
           }
         }
       ]
-    },
-    {
-      path: '/nav',
-      name: 'Navigation',
-      component: Navigation
     },
     {
       path: '/register',
