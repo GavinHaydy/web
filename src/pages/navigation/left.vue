b9fd4eb2369faf832241d4d48798e32e78dbba7c
bugpz <13558622779@163.com>
2021-04-08 17:26:51 +0800

    fix：左边菜单导航，测试布局，暂无类容

---
+++ src/pages/navigation/left.vue
@@ -0,0 +1,30 @@
+<!--
+ * @Description:left.vue
+ * @CreatedBy:WebStorm
+ * @Author: the-ruffian
+ * @Date: 2021-04-08 16:13
+ * @LastEditTime: 2021-4-8 17:25:13
+ * @LastEditors: the-ruffian
+-->
+<template>
+  <div>
+    <el-row>
+      <el-col
+        :span=24
+        style="background-color: #666666; height: 100vh"
+      >
+        菜单导航
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'left'
+}
+</script>
+
+<style scoped>
+
+</style>
