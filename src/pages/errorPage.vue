61e5de10220445d85994fd293783019680f6d4ac
bugpz <13558622779@163.com>
2021-04-10 15:04:18 +0800

    add：错误页相关代码

---
+++ src/pages/errorPage.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Description:errorPage.vue
+ * @CreatedBy:WebStorm
+ * @Author: the-ruffian
+ * @Date: 2021-04-10 14:23
+ * @LastEditTime: 2021-04-10 14:23
+ * @LastEditors: the-ruffian
+-->
+<template>
+  <div id="ErrorPage">
+    <iframe src="../../static/404.html" style="width: 100%;height: 95vh"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorPage'
+}
+</script>
+
+<style scoped></style>
