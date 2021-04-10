ebe2d5116aaee0f9ddbe07d16aaa6c01b915a43c
bugpz <13558622779@163.com>
2021-04-10 17:01:58 +0800

    fix：用户管理页

---
+++ src/pages/setting/authSettings/memberManage/index.vue
@@ -0,0 +1,66 @@
+<!--
+ * @Description:index.vue
+ * @CreatedBy:WebStorm
+ * @Author: the-ruffian
+ * @Date: 2021-04-09 22:38
+ * @LastEditTime: 2021-04-09 22:38
+ * @LastEditors: the-ruffian
+-->
+<template>
+  <div>
+    <div>
+      <el-table
+        :data="userList"
+        stripe="true"
+        border
+        highlight-current-row
+      >
+        <el-table-column
+          label="邮箱"
+          prop="email"
+        ></el-table-column>
+        <el-table-column
+          label="账号"
+          prop="phone"
+        ></el-table-column>
+        <el-table-column
+          label="用户名"
+          prop="username"
+        ></el-table-column>
+        <el-table-column
+          label="性别"
+          prop="sex"
+        ></el-table-column>
+      </el-table>
+    </div>
+  </div>
+</template>
+
+<script>
+import {userFindAll} from '../../../../api/user'
+
+export default {
+  name: 'index',
+  data () {
+    return {
+      userList: []
+    }
+  },
+  created () {
+    this.ajaxFun()
+  },
+  methods: {
+    ajaxFun () {
+      userFindAll()
+        .then(res => {
+          this.userList = res.data
+          console.log(res.data)
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
