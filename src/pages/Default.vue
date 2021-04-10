5fd8c72ab607363ae48c4be7f13b8c06b1607f5c
bugpz <13558622779@163.com>
2021-04-10 15:04:55 +0800

    add：空白继承页

---
+++ src/pages/Default.vue
@@ -0,0 +1,23 @@
+<!--
+ * @Description:Default.vue
+ * @CreatedBy:WebStorm
+ * @Author: the-ruffian
+ * @Date: 2021-04-09 23:04
+ * @LastEditTime: 2021-04-09 23:04
+ * @LastEditors: the-ruffian
+-->
+<template>
+  <div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Default'
+}
+</script>
+
+<style scoped>
+
+</style>
