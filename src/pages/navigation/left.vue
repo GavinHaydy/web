2354bcbe064f50b852687d6ec316ceb8b9ff22d8
bugpz <13558622779@163.com>
2021-04-10 20:06:39 +0800

    fix：完成菜单布局代码

--- src/pages/navigation/left.vue
+++ src/pages/navigation/left.vue
@@ -8,23 +8,97 @@
 -->
 <template>
   <div>
-    <el-row>
+    <el-row
+      class="el_col"
+      :style="{width: $store.state.types.type === 1 ? '201px!important' : '64px!important'}"
+    >
       <el-col
         :span=24
-        style="background-color: #666666; height: 100vh"
+        style="height: 100vh"
       >
-        菜单导航
+        <el-menu
+          style="height: 100vh"
+          :unique-opened="true"
+          :collapse="$store.state.types.type !== 1"
+          :collapse-transition="false"
+          :default-active="$route.path"
+          background-color="#666666"
+          active-text-color="#62d6e2"
+          text-color="#37ff2b"
+          router
+        >
+          <el-menu-item
+            index="/">
+            <svg class="icon" aria-hidden="true">
+              <use xlink:href="#i-home"></use>
+            </svg>
+            <span>首页</span>
+          </el-menu-item>
+          <el-submenu
+            v-for="(idea, index) in routes"
+            :key="index"
+            :index="idea.path"
+          >
+            <template
+              slot="title"
+              style="text-align: center"
+            >
+              <svg
+                class="icon" aria-hidden="true"
+              >
+                <use
+                  :href="idea.meta.icon"></use>
+              </svg>
+              <span>{{idea.meta.title}}</span>
+            </template>
+            <el-submenu
+              :index="idea.path + '/' + idea2.path"
+              v-if="idea.meta.layer === 3 "
+              v-for="(idea2, index) in idea.children"
+              :key="index"
+            >
+              <template
+                slot="title"
+              >{{idea2.meta.title}}</template>
+              <el-menu-item
+                v-for="(idea3, index) in idea2.children"
+                :key="index"
+                :index="idea.path + '/' + idea2.path + '/' + idea3.path"
+              >{{idea3.meta.title}}</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+        </el-menu>
       </el-col>
     </el-row>
+    <div
+      :style="{marginLeft: $store.state.types.type === 1 ? '201px!important' : '64px!important'}"
+    >
+      <Top/>
+    </div>
   </div>
 </template>
 
 <script>
+import setting from '../../router/settings'
+import Top from './top'
 export default {
-  name: 'left'
+  name: 'left',
+  components: {Top},
+  data () {
+    return {
+      routes: {
+        setting
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.icon {
+  width: 18px; height: 18px;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
 </style>
