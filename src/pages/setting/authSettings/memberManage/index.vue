047f0b442214be1432865383c66f010038e38059
bugpz <13558622779@163.com>
2021-04-11 22:01:01 +0800

    fix：增加编辑、删除用户功能，修复已知bug PS(数据库未做判空校验)

--- src/pages/setting/authSettings/memberManage/index.vue
+++ src/pages/setting/authSettings/memberManage/index.vue
@@ -3,7 +3,7 @@
  * @CreatedBy:WebStorm
  * @Author: the-ruffian
  * @Date: 2021-04-09 22:38
- * @LastEditTime: 2021-04-09 22:38
+ * @LastEditTime: 2021-04-11 21:56:35
  * @LastEditors: the-ruffian
 -->
 <template>
@@ -29,19 +29,115 @@
           label="性别"
           prop="sex"
         ></el-table-column>
+        <el-table-column
+          label="操作"
+          fixed="right"
+        >
+          <template slot-scope="{row}">
+            <el-button
+              type="text"
+              size="mini"
+              @click="handleEdit(row)"
+            >编辑</el-button>
+            <el-button
+              type="text"
+              size="mini"
+              @click="handleDelete(row)"
+            >删除</el-button>
+          </template>
+        </el-table-column>
       </el-table>
     </div>
+    <el-dialog
+      title="编辑用户"
+      :visible.sync="dialogFormVisible"
+    >
+      <el-form
+        :model="form"
+      >
+        <el-form-item
+          label="用户名"
+        >
+          <el-input
+            v-model="form.username"
+          ></el-input>
+        </el-form-item>
+        <el-form-item
+          label="密码"
+        >
+          <el-input
+            show-password
+            v-model="form.password"
+          ></el-input>
+        </el-form-item>
+        <el-form-item
+          label="确认密码"
+        >
+            <el-input
+              show-password
+              v-model="form.rePassword"
+            ></el-input>
+        </el-form-item>
+        <el-form-item
+          label="手机号"
+        >
+          <el-input
+            v-model="form.phone"
+            disabled
+          ></el-input>
+        </el-form-item>
+        <el-form-item
+          label="性别"
+        >
+          <el-input
+            disabled
+            v-model="form.sex"
+          ></el-input>
+        </el-form-item>
+        <el-form-item
+          label="邮箱"
+        >
+          <el-input
+            disabled
+            v-model="form.email"
+          ></el-input>
+        </el-form-item>
+      </el-form>
+      <div
+        slot="footer"
+        class="dialog-footer"
+      >
+        <el-button
+          size="mini"
+          @click="dialogFormVisible = false"
+        >取消</el-button>
+        <el-button
+          size="mini"
+          type="primary"
+          @click="handleUpdate"
+        >保存</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
 <script>
-import {userFindAll} from '../../../../api/user'
+import {userDelete, userFindAll, userUpdate} from '../../../../api/user'
 
 export default {
   name: 'index',
   data () {
     return {
-      userList: []
+      userList: [],
+      dialogFormVisible: false,
+      form: {
+        username: '',
+        phone: '',
+        password: '',
+        rePassword: '',
+        sex: '',
+        email: ''
+      }
     }
   },
   created () {
@@ -51,9 +147,47 @@ export default {
     ajaxFun () {
       userFindAll()
         .then(res => {
-          this.userList = res.data
+          this.userList = res.data.result
           console.log(res.data)
         })
+    },
+    handleDelete (row) {
+      userDelete({phone: row.phone})
+        .then(res => {
+          if (res.data.code !== 200) {
+            this.$message.error(res.data.msg)
+          } else {
+            this.$message.success(res.data.msg)
+          }
+          setTimeout(function () { location.reload() }, 1800)
+        })
+    },
+    handleEdit (row) {
+      this.dialogFormVisible = true
+      this.form.phone = row.phone
+      this.form.sex = row.sex
+      this.form.email = row.email
+      this.form.username = row.username
+    },
+    handleUpdate () {
+      if (this.form.password === this.form.rePassword) {
+        userUpdate({
+          phone: this.form.phone,
+          password: this.$md5(this.form.password),
+          username: this.form.username,
+          sex: this.form.sex,
+          email: this.form.email})
+          .then(res => {
+            if (res.data.code === 200) {
+              this.$message.success(res.data.msg)
+              this.dialogFormVisible = false
+            } else {
+              this.$message.error(res.data.msg)
+            }
+          })
+      } else if (this.form.password !== this.form.rePassword) {
+        this.$message.error('两次输入的密码不一致，请重新输入')
+      }
     }
   }
 }
