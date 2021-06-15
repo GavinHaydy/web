c92f4fd0798267b38dd3fbe0ddfc5b3ecfa8ca32
bugpz <13558622779@163.com>
2021-06-15 16:21:27 +0800

    fix：修改时间戳转换后，格式不统一的bug

--- src/utils/parseTime.js
+++ src/utils/parseTime.js
@@ -10,9 +10,9 @@ export function parseTime (time) {
   let date = new Date(time) // 时间戳为10位需*1000，时间戳为13位的话不需乘1000
   let Y = date.getFullYear() + '-'
   let M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-' // getMonth范围0-11
-  let D = date.getDate() < 10 ? '0' + date.getDate() : date.getDate() + ' '
-  let h = date.getHours() < 10 ? '0' + date.getHours() : date.getHours() + ':'
-  let m = date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes() + ':'
+  let D = (date.getDate() < 10 ? '0' + date.getDate() : date.getDate()) + ' '
+  let h = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':'
+  let m = (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) + ':'
   let s = date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds()
   return Y + M + D + h + m + s
 }
