1e79c3b6fdf9ae7b922c1ae1336875525084ce65
bugpz <13558622779@163.com>
2021-06-30 20:09:18 +0800

    fix：增加表单校验

--- src/pages/login/register.vue
+++ src/pages/login/register.vue
@@ -13,13 +13,16 @@
     <el-form
       class="register_form"
       inline="inline"
+      :model="ruleForm"
+      :rules="rules"
     >
       <el-row>
         <el-form-item
           label="电话"
+          prop="phone"
         >
           <el-input
-            v-model="phone"
+            v-model="ruleForm.phone"
             maxlength="11"
           ></el-input>
         </el-form-item>
@@ -27,9 +30,10 @@
       <el-row>
         <el-form-item
           label="昵称"
+          prop="user"
         >
           <el-input
-            v-model="user"
+            v-model="ruleForm.user"
             maxlength="20"
           ></el-input>
         </el-form-item>
@@ -37,9 +41,10 @@
       <el-row>
         <el-form-item
           label="密码"
+          prop="password"
         >
           <el-input
-            v-model="password"
+            v-model="ruleForm.password"
             show-password
             maxlength="20"
           ></el-input>
@@ -48,9 +53,10 @@
       <el-row>
         <el-form-item
           label="确认密码"
+          prop="rePass"
         >
           <el-input
-            v-model="rePass"
+            v-model="ruleForm.rePass"
             show-password
             maxlength="20"
           ></el-input>
@@ -59,21 +65,32 @@
       <el-row>
         <el-form-item
           label="邮箱"
+          prop="email"
         >
           <el-input
-            v-model="email"
+            v-model="ruleForm.email"
             type="email"
-            maxlength="20"
+            maxlength="30"
           ></el-input>
         </el-form-item>
       </el-row>
       <el-row>
         <el-form-item
           label="性别"
+          prop="gender"
         >
-          <el-input
-            v-model="sex"
-          ></el-input>
+          <el-select
+            placeholder="请选择"
+            clearable
+            v-model="ruleForm.genderI"
+          >
+            <el-option
+              v-for="(gender, index) in ruleForm.gender"
+              :key="index"
+              :label="gender.label"
+              :value="gender.value"
+            ></el-option>
+          </el-select>
         </el-form-item>
       </el-row>
       <el-row>
@@ -92,13 +109,46 @@ import {userRegister} from '../../api/user'
 export default {
   name: 'register',
   data () {
+    const checkPhone = (rule, value, callback) => {
+      if (!this.$checkPhone.test(value)) {
+        callback(new Error('请填写正确的手机号'))
+      }
+    }
+    const checkPassword = (rule, value, callback) => {
+      if (!this.$checkPassword.test(value)) {
+        callback(new Error('密码8-20位，支持英文、数字'))
+      }
+    }
+    const checkEmail = (rule, value, callback) => {
+      if (!this.$checkEmail.test(value)) {
+        callback(new Error('请填写正确的邮箱'))
+      }
+    }
+    const checkRepass = (rule, value, callback) => {
+      if (value !== this.ruleForm.password) {
+        callback(new Error('两次密码不一致，请重新输入'))
+      }
+    }
     return {
-      phone: '',
-      user: '',
-      sex: '',
-      password: '',
-      rePass: '',
-      email: ''
+      ruleForm: {
+        phone: '',
+        user: '',
+        genderI: '',
+        gender: [
+          {value: 0, label: '女'},
+          {value: 1, label: '男'},
+          {value: 2, label: '保密'}
+        ],
+        password: '',
+        rePass: '',
+        email: ''
+      },
+      rules: {
+        phone: [{validator: checkPhone, trigger: 'blur'}],
+        password: [{validator: checkPassword, trigger: 'blur'}],
+        email: [{validator: checkEmail, trigger: 'blur'}],
+        rePass: [{validator: checkRepass, trigger: 'blur'}]
+      }
     }
   },
   methods: {
@@ -107,7 +157,7 @@ export default {
       userRegister({
         'username': this.user,
         'phone': this.phone,
-        'sex': this.sex,
+        'gender': this.gender,
         'password': md5Password,
         'email': this.email
       })
