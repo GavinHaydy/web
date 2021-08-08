01fbe2604aca67c98348bc88f0f9e9e6d71db50a
the-ruffian <13558622779@163.com>
2021-08-08 20:58:56 +0800

    add：新增验证码正则校验

--- src/global.js
+++ src/global.js
@@ -10,11 +10,13 @@ import Vue from 'vue'
 const checkPhone = /^1[34578][0-9]{9}$/
 const checkPassword = /^[A-Za-z0-9]{8,20}$/
 const checkEmail = /^[a-zA-Z0-9_-]{6,20}@[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)+$/
+const checkCode = /^[0-9]{4}$/
 
 export default {
   install () {
     Vue.prototype.$checkPhone = checkPhone
     Vue.prototype.$checkPassword = checkPassword
     Vue.prototype.$checkEmail = checkEmail
+    Vue.prototype.$checkCode = checkCode
   }
 }
