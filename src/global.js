3a6d5d81ce07ca780fbd6c8f05d21b90a5d407b5
bugpz <13558622779@163.com>
2021-08-11 21:58:19 +0800

    add：增加校验空格正则

--- src/global.js
+++ src/global.js
@@ -3,7 +3,7 @@
  * @CreatedBy:WebStorm
  * @Author: BugP
  * @Date: 2021-06-30 11:13
- * @LastEditTime: 2021-06-30 11:13
+ * @LastEditTime: 2021-8-11 21:57:54
  * @LastEditors: BugP
 */
 import Vue from 'vue'
@@ -11,6 +11,7 @@ const checkPhone = /^1[34578][0-9]{9}$/
 const checkPassword = /^[A-Za-z0-9]{8,20}$/
 const checkEmail = /^[a-zA-Z0-9_-]{6,20}@[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)+$/
 const checkCode = /^[0-9]{4}$/
+const checkNull = /\s/
 
 export default {
   install () {
@@ -18,5 +19,6 @@ export default {
     Vue.prototype.$checkPassword = checkPassword
     Vue.prototype.$checkEmail = checkEmail
     Vue.prototype.$checkCode = checkCode
+    Vue.prototype.$checkNull = checkNull
   }
 }
