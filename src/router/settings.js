fb91ed2f6a5a32e5cc78b4afc9b22cf2f5dfec84
bugpz <13558622779@163.com>
2021-08-22 18:52:26 +0800

    fix：修改角色管理路由

--- src/router/settings.js
+++ src/router/settings.js
@@ -3,12 +3,13 @@
  * @CreatedBy:WebStorm
  * @Author: the-ruffian
  * @Date: 2021-04-09 22:48
- * @LastEditTime: 2021-04-09 22:48
+ * @LastEditTime: 2021-08-22 18:52:00
  * @LastEditors: the-ruffian
 */
 import memberManage from '../pages/setting/authSettings/memberManage'
 import Default from '../pages/Default'
 import roleManage from '@/pages/setting/authSettings/roleManage'
+import authority from '../pages/setting/authSettings/roleManage/authority'
 const setting = {
   path: '/setting',
   meta: {title: '系统设置', icon: '#i-system', layer: 3},
@@ -26,7 +27,15 @@ const setting = {
     }, {
       path: 'roleManage',
       meta: {title: '角色管理'},
-      component: roleManage
+      component: Default,
+      redirect: '',
+      children: [{
+        path: '',
+        component: roleManage
+      }, {
+        path: 'permission/:roleName',
+        component: authority
+      }]
     }
     ]
   }]
