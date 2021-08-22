ddd9f7b9497c42e5a0af7bbcd424eb530ea6522b
bugpz <13558622779@163.com>
2021-08-22 18:53:25 +0800

    add：角色权限配置页（待完善）

---
+++ src/pages/setting/authSettings/roleManage/authority.vue
@@ -0,0 +1,52 @@
+<!--
+ * @Description:权限配置页面
+ * @CreatedBy:WebStorm
+ * @Author: the-ruffian
+ * @Date: 2021-08-22 17:52
+ * @LastEditTime: 2021-08-22 17:52
+ * @LastEditors: the-ruffian
+-->
+<template>
+  <div>
+    <el-tree
+      :data="form"
+      :props="props"
+      show-checkbox
+    >
+    </el-tree>
+  </div>
+</template>
+
+<script>
+import {permission} from '../../../../api/role'
+
+export default {
+  name: 'authority',
+  data () {
+    return {
+      form: {},
+      props: {
+        label: 'permissionName',
+        children: 'children'
+      }
+    }
+  },
+  created () {
+    this.ajaxFun()
+  },
+  methods: {
+    ajaxFun () {
+      permission({
+      }).then(res => {
+        if (res.data.code === 200) {
+          this.form = res.data.result
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
